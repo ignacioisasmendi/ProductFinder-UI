200fb2d9f616509e79e805cd5a64962d311cfb1d
Nacho <isasmendi.ignacio@gmail.com>
2024-08-20 14:18:06 -0300

    Small updates

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,40 +8,11 @@ import mercadoLibre from '../assets/mercadoLibre.svg';
 import Divider from '@mui/material/Divider';
 import { DataGrid } from '@mui/x-data-grid';
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-
 
 export default function ProductCard () {
+
+  
+
   return (
     <Grid item xs={4}>
       <Paper elevation={1}>
@@ -55,16 +26,20 @@ export default function ProductCard () {
         >
           <img className="img" src="https://http2.mlstatic.com/D_NQ_NP_951036-MLA47845059413_102021-O.webp" alt="" />
           <Box padding={1}>
-            <Typography variant="subtitle1" component="h2">
+            <Typography variant="h6" component="h6">
               Creatine BSN 300g 
             </Typography>
           </Box>
           <Divider flexItem />
           <Box>
-            <Typography sx={{ paddingTop:'8px' }} variant="h5" component="p">
+            <Typography sx={{ paddingTop:'8px' }} variant="subtitle1" component="p">
               Profit: $ 100.00
             </Typography>
+            <Typography sx={{ paddingTop:'8px' }} variant="subtitle1" component="p">
+              Weight: 0.3 kg
+            </Typography>
           </Box>
+          <Divider flexItem />
           <Box 
             sx={{
               display:"flex",
